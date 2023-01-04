ad08f7a7951f82f175ebe736c4c02dea52764b96
chahirchalouati <annabersano7@gmail,com>
2023-01-04 13:41:36 +0100

    2023.01.04

--- infrastructure/scripts/infrastructure_setup.sh
+++ infrastructure/scripts/infrastructure_setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 #eval "../../mvnw -B --file ../../pom.xml -s ../../settings.xml  -DskipTests=true clean install"
-docker-compose -f ../docker/docker-compose-dev.yaml -f ../docker/docker-compose-services.yaml -p microservices up -d --build
+docker-compose -f ../docker/docker-compose-dev.yaml -f ../docker/docker-compose-services.yaml -p microservices up -d
