cfeddfc35ad3d55d6bfd43614413fc9823b081ab
chahirchalouati <annabersano7@gmail,com>
2023-01-06 15:02:38 +0100

    2023.01.06

---
+++ clients/gramify/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {ApiInterceptor} from "./shared/interceptors/api.interceptor";
+import {JwtInterceptor} from "./shared/interceptors/jwt.interceptor";
+import {HomeComponent} from "./pages/home/home.component";
+import {AuthorizationComponent} from "./pages/authorization/authorization.component";
+import {ErrorComponent} from "./pages/error/error.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AuthorizationComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: ApiInterceptor,
+    multi: true,
+  },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    }],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
