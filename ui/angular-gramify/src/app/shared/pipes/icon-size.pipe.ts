e517a1e3bd4f45c59172946e019b1b7813a6802f
chahirchalouati <annabersano7@gmail,com>
2023-01-24 10:54:30 +0100

    2023.01.23

--- ui/angular-gramify/src/app/shared/pipes/icon-size.pipe.ts
+++ ui/angular-gramify/src/app/shared/pipes/icon-size.pipe.ts
@@ -6,7 +6,7 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class IconSizePipe implements PipeTransform {
 
   transform(value: string, size: number): string {
-    return value.replace('height="20" width="20"', `height="${size}" width="${size}"`);
+    return value.replace(`height="20" width="20"`, `height="${size}" width="${size}"`);
   }
 
 }
