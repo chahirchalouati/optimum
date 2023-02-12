99b6657b46e65a426d131da8e7752cd60073cb7f
chahirchalouati <annabersano7@gmail,com>
2023-02-12 19:43:08 +0100

    2023.02.12

--- infrastructure/docker/entrypoint.sh
+++ infrastructure/docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
 
