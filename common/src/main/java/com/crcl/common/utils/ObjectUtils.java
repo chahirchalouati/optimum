c7aeb022b44b79ab782d315f14bbd87fefc90f02
Chahir Chalouati <chehhhir@gmail.com>
2023-02-19 10:36:07 +0100

    19.02.2023

--- common/src/main/java/com/crcl/common/utils/ObjectUtils.java
+++ common/src/main/java/com/crcl/common/utils/ObjectUtils.java
@@ -1,7 +1,10 @@
 package com.crcl.common.utils;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class ObjectUtils {
     public static <V> void setIfNotNull(V value, Consumer<V> setter) {
@@ -9,4 +12,27 @@ public class ObjectUtils {
             setter.accept(value);
         }
     }
+
+    public static <V> void throwIfNull(V value, Supplier<V> setter) {
+        if (Objects.isNull(value)) {
+            setter.get();
+        }
+    }
+
+    public static <T> void testAndThrow(T value, Predicate<T> predicate, Class<? extends RuntimeException> exceptionType, String message) {
+        if (predicate.test(value)) {
+            try {
+                throw exceptionType.getConstructor(String.class).newInstance(message);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException("Failed to throw exception of type " + exceptionType.getName(), e);
+            }
+        }
+    }
+
+//    /*
+//       Debugging utilities
+//     */
+//    public static void main(String[] args) {
+//        testAndThrow(null, Objects::isNull, IllegalArgumentException.class, "value is null");
+//    }
 }
