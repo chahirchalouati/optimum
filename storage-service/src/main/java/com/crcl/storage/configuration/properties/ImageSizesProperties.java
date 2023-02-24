9eb32a1b380effacaa022b5ffbd050623cd358c7
Chahir Chalouati <chehhhir@gmail.com>
2023-02-24 22:18:25 +0100

    24.02.2023

--- storage-service/src/main/java/com/crcl/storage/configuration/properties/ImageSizesProperties.java
+++ storage-service/src/main/java/com/crcl/storage/configuration/properties/ImageSizesProperties.java
@@ -15,6 +15,7 @@ public class ImageSizesProperties {
     public static class ImageSize {
         private int width;
         private int height;
+        private String name;
 
         public String getLabel() {
             return "_" + this.width + "_" + this.height;
