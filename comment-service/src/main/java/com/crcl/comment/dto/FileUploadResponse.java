60408556fe92fd1d5868928f5c9a0c8e678d8f8a
Chahir Chalouati <chehhhir@gmail.com>
2023-05-07 12:09:23 +0200

    Fix: code refactor & clean-up

--- comment-service/src/main/java/com/crcl/comment/dto/FileUploadResponse.java
+++ comment-service/src/main/java/com/crcl/comment/dto/FileUploadResponse.java
@@ -1,23 +1,13 @@
-<<<<<<<< HEAD:post-service/src/main/java/com/crcl/post/dto/FileUploadResult.java
-package com.crcl.post.dto;
-
-========
 
 package com.crcl.comment.dto;
->>>>>>>> develop-v2:comment-service/src/main/java/com/crcl/comment/dto/FileUploadResponse.java
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
-<<<<<<<< HEAD:post-service/src/main/java/com/crcl/post/dto/FileUploadResult.java
-public class FileUploadResult {
-    private String etag;
-========
 public class FileUploadResponse {
   private String etag;
->>>>>>>> develop-v2:comment-service/src/main/java/com/crcl/comment/dto/FileUploadResponse.java
     private String name;
     private String bucket;
     private String version;
