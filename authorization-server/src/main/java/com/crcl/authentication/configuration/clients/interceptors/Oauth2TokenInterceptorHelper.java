b66af32d8c08542a46b249c1dd1686526fbbcbd6
Chahir Chalouati <chehhhir@gmail.com>
2023-05-27 16:09:26 +0200

    Feat: add ci/cd for services

--- authorization-server/src/main/java/com/crcl/authentication/configuration/clients/interceptors/Oauth2TokenInterceptorHelper.java
+++ authorization-server/src/main/java/com/crcl/authentication/configuration/clients/interceptors/Oauth2TokenInterceptorHelper.java
@@ -1,14 +1,11 @@
 package com.crcl.authentication.configuration.clients.interceptors;
 
-import com.crcl.common.configuration.properties.AuthorizationProperties;
+import com.crcl.authentication.configuration.props.AuthorizationProperties;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -26,15 +23,18 @@ public class Oauth2TokenInterceptorHelper {
 
     public String getClientAccessToken(String clientId, String password) {
         try {
-            if (StringUtils.isBlank(clientId) || StringUtils.isBlank(password)) return StringUtils.EMPTY;
-            var formValues = new LinkedMultiValueMap<>();
+            if (StringUtils.isBlank(clientId) || StringUtils.isBlank(password))
+                return StringUtils.EMPTY;
+
+            LinkedMultiValueMap<Object, Object> formValues = new LinkedMultiValueMap<>();
             formValues.add(GRANT_TYPE, CLIENT_CREDENTIALS.getValue());
             formValues.add(CLIENT_ID, clientId);
             formValues.add(CLIENT_SECRET, password);
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
             HttpEntity<?> request = new HttpEntity<>(formValues, headers);
-            var response = restTemplate.exchange(authorizationProperties.getTokenUrl(), HttpMethod.POST, request, String.class);
+            ResponseEntity<String> response = restTemplate.exchange(authorizationProperties.getTokenUrl(), HttpMethod.POST, request, String.class);
+
             return new JSONObject(response.getBody()).getString("access_token");
         } catch (JSONException e) {
             return StringUtils.EMPTY;
