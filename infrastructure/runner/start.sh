2a9115ba15b08d4ee0306aca922104d635f2fcd2
Chahir Chalouati <chalouati.chahir@gmail.com>
2023-07-23 19:24:49 +0200

    Fix: update runner configuration

--- infrastructure/runner/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-gitlab-runner register --non-interactive ${REGISTER_NON_INTERACTIVE} --url ${CI_SERVER_URL} --description ${DESCRIPTION} --tag-list ${RUNNER_TAG_LIST} --registration-token ${REGISTRATION_TOKEN} --executor ${RUNNER_EXECUTOR} --docker-image ${DOCKER_IMAGE} --docker-network-mode ${DOCKER_NETWORK_MODE} --docker-volumes ${DOCKER_VOLUMES} --docker-privileged ${DOCKER_PRIVILEGED}
-
-gitlab-runner run
