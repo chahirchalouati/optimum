f791631882065a1193e88e35efea0f5c2cc979d1
chahirchalouati <chehhhir@gmail.com>
2023-10-21 11:18:35 +0200

    Feat: add Friendship service

--- friend-ship/src/main/java/com/crcl/friend_ship/repository/CustomFriendShipRepositoryImpl.java
+++ friend-ship/src/main/java/com/crcl/friend_ship/repository/CustomFriendShipRepositoryImpl.java
@@ -2,12 +2,11 @@ package com.crcl.friend_ship.repository;
 
 import com.crcl.friend_ship.domain.Friendship;
 import lombok.RequiredArgsConstructor;
-import org.neo4j.cypherdsl.core.Cypher;
-import org.neo4j.cypherdsl.core.ResultStatement;
-import org.neo4j.cypherdsl.core.Statement;
+import org.neo4j.cypherdsl.core.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.neo4j.core.ReactiveNeo4jOperations;
 import org.springframework.data.neo4j.core.ReactiveNeo4jTemplate;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -18,18 +17,18 @@ import java.util.List;
 @Repository
 @RequiredArgsConstructor
 public class CustomFriendShipRepositoryImpl implements CustomFriendShipRepository {
-    private final ReactiveNeo4jTemplate reactiveNeo4jTemplate;
+    private final ReactiveNeo4jOperations reactiveNeo4jOperations;
 
     @Override
     public Mono<Page<Friendship>> findAll(Pageable pageable) {
-        final ResultStatement query = Cypher.match(Cypher.node("f", "Friendship"))
+        final ResultStatement query = Cypher.match(Cypher.node("friendship").named("f"))
                 .returning("f")
                 .skip(Cypher.literalOf(pageable.getOffset()))
                 .limit(Cypher.literalOf(pageable.getPageSize()))
                 .build();
 
-       final Flux<Friendship> friendships = reactiveNeo4jTemplate.findAll(query, Friendship.class);
-       final Mono<Long> totalFriendsMono = reactiveNeo4jTemplate.count(Friendship.class);
+        final Flux<Friendship> friendships = reactiveNeo4jOperations.findAll(query, Friendship.class);
+        final Mono<Long> totalFriendsMono = reactiveNeo4jOperations.count(Friendship.class);
 
         return Mono.zip(totalFriendsMono, friendships.collectList())
                 .map(tuple -> {
