450cfd7f78707fc928fb394727fbdeb5843b2898
chahirchalouati <chehhhir@gmail.com>
2023-10-22 17:39:54 +0200

    add message  service

---
+++ message/src/main/java/com/crcl/message/MessageServiceApplication.java
@@ -0,0 +1,34 @@
+package com.crcl.message;
+
+import org.neo4j.cypherdsl.core.renderer.Configuration;
+import org.neo4j.cypherdsl.core.renderer.Dialect;
+import org.neo4j.driver.Driver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.neo4j.core.transaction.ReactiveNeo4jTransactionManager;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.ReactiveTransactionManager;
+
+@EnableNeo4jRepositories("com.crcl.message.repository")
+@ConfigurationPropertiesScan("com.crcl.message.configuration.*")
+@SpringBootApplication
+@EnableFeignClients
+public class FriendShipServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FriendShipServiceApplication.class, args);
+    }
+    @Bean
+    public ReactiveTransactionManager reactiveTransactionManager(Driver driver) {
+        return  new ReactiveNeo4jTransactionManager(driver);
+    }
+    @Bean
+    Configuration cypherDslConfiguration() {
+        return Configuration.newConfig()
+                .withDialect(Dialect.NEO4J_5).build();
+    }
+}
