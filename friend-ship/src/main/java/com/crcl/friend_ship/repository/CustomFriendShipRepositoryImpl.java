6b5a17b4907e496e386bad102a18eb8bdb72754f
chahirchalouati <chehhhir@gmail.com>
2023-10-22 17:43:08 +0200

    Feat: add Message service

--- friend-ship/src/main/java/com/crcl/friend_ship/repository/CustomFriendShipRepositoryImpl.java
+++ friend-ship/src/main/java/com/crcl/friend_ship/repository/CustomFriendShipRepositoryImpl.java
@@ -42,6 +42,21 @@ public class CustomFriendShipRepositoryImpl implements CustomFriendShipRepositor
 
     @Override
     public Mono<Page<Friendship>> findNonFriend(String username, Pageable pageable) {
-        return null;
+        // TODO: 22/10/23 update the query to retrieve users correctly
+        final ResultStatement query = Cypher.match(Cypher.node("friendship").named("f"))
+                .returning("f")
+                .skip(Cypher.literalOf(pageable.getOffset()))
+                .limit(Cypher.literalOf(pageable.getPageSize()))
+                .build();
+
+        final Flux<Friendship> friendships = reactiveNeo4jOperations.findAll(query, Friendship.class);
+        final Mono<Long> totalFriendsMono = reactiveNeo4jOperations.count(Friendship.class);
+
+        return Mono.zip(totalFriendsMono, friendships.collectList())
+                .map(tuple -> {
+                    Long totalFriends = tuple.getT1();
+                    List<Friendship> friendshipList = tuple.getT2();
+                    return new PageImpl<>(friendshipList, pageable, totalFriends);
+                });
     }
 }
